64e8bbe514f07428ef068d0cf196899f41f5d7af
Bailang <bailang0324@outlook.com>
2018-08-04 09:53:21 +0800

    Added dependancies.

---
+++ src/AzureStorageServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Matthewbdaly\LaravelAzureStorage;
+
+use Storage;
+use League\Flysystem\Filesystem;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Azure\AzureAdapter;
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+
+/**
+ * Service provider for Azure Blob Storage
+ */
+class AzureStorageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('azure', function ($app, $config) {
+            $connectionString = sprintf(
+                'DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s',
+                $config['name'],
+                $config['key']
+            );
+            $blobClient = BlobRestProxy::createBlobService($connectionString);
+            return new Filesystem(new AzureAdapter($blobClient, "/".$config['container']));
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
